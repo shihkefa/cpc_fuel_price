a57c33befee2ca0131d26b73b09d8806dedadce2
shihkefa <120175626+shihkefa@users.noreply.github.com>
2025-10-13 12:50:54 +0800

    Update const.py

--- custom_components/cpc_fuel_price/const.py
+++ custom_components/cpc_fuel_price/const.py
@@ -1,2 +1,9 @@
 DOMAIN = "cpc_fuel_price"
 API_URL = "https://vipmbr.cpc.com.tw/opendata/mainprodlistprice"
+
+# 三種油品
+PRODUCTS = [
+    {"name": "CPC Product 98 Prices", "index": 0},
+    {"name": "CPC Product 95 Prices", "index": 1},
+    {"name": "CPC Product 92 Prices", "index": 2},
+]
