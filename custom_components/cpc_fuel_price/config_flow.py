9de2882d3fdef7b097cfe68c159ea5861ef3a7e3
shihkefa <120175626+shihkefa@users.noreply.github.com>
2025-10-13 12:51:32 +0800

    Update config_flow.py

--- custom_components/cpc_fuel_price/config_flow.py
+++ custom_components/cpc_fuel_price/config_flow.py
@@ -12,19 +12,3 @@ class CPCFuelPriceConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             return self.async_create_entry(title="CPC Fuel Price", data={})
         return self.async_show_form(step_id="user", data_schema=None)
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        return CPCFuelPriceOptionsFlow(config_entry)
-
-class CPCFuelPriceOptionsFlow(config_entries.OptionsFlow):
-    """Options flow for CPC Fuel Price."""
-
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        if user_input is not None:
-            return self.async_create_entry(title="", data={})
-        return self.async_show_form(step_id="init", data_schema=None)
