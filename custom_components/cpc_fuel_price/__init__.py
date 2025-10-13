1e9135734771cef7d57eb695e4f3e2dd358d57b5
shihkefa <120175626+shihkefa@users.noreply.github.com>
2025-10-13 12:46:48 +0800

    Update __init__.py

--- custom_components/cpc_fuel_price/__init__.py
+++ custom_components/cpc_fuel_price/__init__.py
@@ -10,12 +10,9 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up CPC Fuel Price from a config entry."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload CPC Fuel Price entry."""
-    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-
+    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
